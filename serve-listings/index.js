1c563db1544fec0086cdbc0d64988a98d2c79411
angusbayley <angusbayley@gmail.com>
2018-12-18 20:16:12 +0000

    define pgPool in global scope so as not to create extra long-lived connections

---
+++ serve-listings/index.js
@@ -0,0 +1,34 @@
+const pg = require('pg');
+let pgPool;
+
+const connectionName =
+  process.env.INSTANCE_CONNECTION_NAME;
+const dbUser = process.env.SQL_USER;
+const dbPassword = process.env.SQL_PASSWORD;
+const dbName = process.env.SQL_NAME;
+
+const pgConfig = {
+  max: 1,
+  user: dbUser,
+  password: dbPassword,
+  database: dbName,
+  host: `/cloudsql/${connectionName}`
+};
+
+const extractQuery = "SELECT url, posted_at, location, price FROM listings LIMIT 5;"
+
+if (!pgPool) {
+    pgPool = new pg.Pool(pgConfig);
+}
+
+exports.serveListings = async (req, res) => {
+
+    pgPool.query(extractQuery, (err, results) => {
+        if (err) {
+            console.error(err);
+            res.send(err);
+        } else {
+            res.send(JSON.stringify(results.rows));
+        }
+    })
+}
