0e6a52cb27a8aa81bd3f98459bd2c350c221ac00
angusbayley <angusbayley@gmail.com>
2018-12-19 23:02:01 +0000

    title element fix

--- view/src/index.js
+++ view/src/index.js
@@ -2,7 +2,7 @@ Vue.component("page-layout", {
   props: ['listings'],
   template: `
     <div class="container">
-        <div class="title">Hackney Wick spaces all-posts-in-one-page megalist 2000!!!!1</div>
+        <h1>Hackney Wick spaces all-posts-in-one-page megalist 2000!!!!1</h1>
             <table class="posts">
                 <tr>
                     <th>give me the link then!!!!</th>
