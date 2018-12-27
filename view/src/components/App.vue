80d76feaee3558620817cdd8a5ee117f65235d4f
angusbayley <angusbayley@gmail.com>
2018-12-27 11:01:43 +0000

    remove old app component

--- view/src/components/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="container">
-        <div class="title">Hackney Wick spaces all-posts-in-one-page megalist 2000!!!!1</div>
-        <div class="posts">
-            <div class="time-partition"></div>
-        </div>
-    </div>
-</template>
